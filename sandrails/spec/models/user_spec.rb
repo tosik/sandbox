1ec3c973befd929cb9a7770118e17115dd77bca4
Toshiyuki Hirooka <toshi.hirooka@gmail.com>
2011-04-14 21:02:06 +0900

    Revert "お金をあまり持っていないときのテストを追加"
    This reverts commit 4ce240af4d1018bfb3123d0c437717b046ab7af9.

--- sandrails/spec/models/user_spec.rb
+++ sandrails/spec/models/user_spec.rb
@@ -1,20 +1,9 @@
-# encoding: utf-8
 require 'spec_helper'
 
 describe User do
   describe '.happy?' do
-    let(:alice) { User.new }
-
-    describe 'お金をたくさん持っている' do
-      before { alice.money = 1500 }
-      subject { alice.happy? }
-      it { should be_true }
-    end
-
-    describe 'お金をあまり持っていない' do
-      before { alice.money = 500 }
-      subject { alice.happy? }
-      it { should be_false }
-    end
+    let(:alice) { User.new(:money => 1500) }
+    subject { alice.happy? }
+    it { should be_true }
   end
 end
