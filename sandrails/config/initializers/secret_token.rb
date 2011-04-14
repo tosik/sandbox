2d54a145ecc6de732bc740a2bcca236176d12b3b
Toshiyuki Hirooka <toshi.hirooka@gmail.com>
2011-04-14 19:00:40 +0900

    rails 作った。rspec な環境。

---
+++ sandrails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sandrails::Application.config.secret_token = '64636ddff0cbe591a65b730e31d8a7d610a31e4cb32e0d03fdf87cdfae054d21731f18ab5fb79c6451b130fa9afb4db9ae87d6339f0f9451bc7c2989890658c7'
